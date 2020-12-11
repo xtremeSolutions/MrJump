4b9bd59d91a3f6b41245addfe9b9c14c75e33e5e
Arslan Goraya <50945026+xtremeSolutions@users.noreply.github.com>
2020-12-11 12:45:24 +0500

    b script added

--- MrJump/Assets/Scripts/b.cs
+++ MrJump/Assets/Scripts/b.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class a : MonoBehaviour
+public class b : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
