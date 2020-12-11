444aba1e7ba6e0a7f3e31fe4a6d4930cc1804f1a
Arslan Goraya <50945026+xtremeSolutions@users.noreply.github.com>
2020-12-11 15:27:40 +0500

    dioe

---
+++ MrJump/Assets/Scripts/alpha.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alpha : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // this script is located in the alpha brach on mr jump and the name of script is alpha
+
+}
